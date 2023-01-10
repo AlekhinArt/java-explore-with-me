b9c30a934d1bb71d170dc83eecd8423368449e62
Artem024 <alekhinart@yandex.ru>
2023-01-10 23:12:49 +0300

    clean

--- ewm-stats-service/src/test/java/ru/practicum/AppTest.java
+++ ewm-stats-service/src/test/java/ru/practicum/AppTest.java
@@ -14,8 +14,7 @@ public class AppTest extends TestCase {
     public static Test suite() {
         return new TestSuite(AppTest.class);
     }
-
-   
+    
     public void testApp() {
         assertTrue(true);
     }
